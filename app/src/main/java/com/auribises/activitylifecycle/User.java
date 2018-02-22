a617961dd98d3c854baa31346d8eb237116c6532
Ishant Kumar <er.ishant@gmail.com>
2018-02-22 12:03:11 +0530

    LocationManager

---
+++ app/src/main/java/com/auribises/activitylifecycle/User.java
@@ -0,0 +1,22 @@
+package com.auribises.activitylifecycle;
+
+/**
+ * Created by ishantkumar on 22/02/18.
+ */
+
+public class User {
+
+    int image;
+    String name;
+    String email;
+
+    public User(){
+
+    }
+
+    public User(int image, String name, String email) {
+        this.image = image;
+        this.name = name;
+        this.email = email;
+    }
+}
